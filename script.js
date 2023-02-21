5578f4970ab1d151e6989b813deca1c7641e6834
Olya Ionga <olechka2888@gmail.com>
2023-02-21 19:52:55 +0200

    update

---
+++ script.js
@@ -0,0 +1,65 @@
+let birthYear = prompt("Введіть рік Вашого народження:");
+let city = prompt("Введіть місто, в якому Ви живете:");
+let favoriteSport = prompt("Введіть Ваш улюблений вид спорту:");
+
+let message = "";
+let champion = "";
+
+switch (city) {
+  case "Київ":
+    message = "Ти живеш у столиці України.";
+    break;
+  case "Лондон":
+    message = "Ти живеш у столиці Великої Британії.";
+    break;
+  case "Вашингтон":
+    message = "Ти живеш у столиці США.";
+    break;
+  default:
+    message = "Ти живеш у місті " + city + ".";
+}
+
+switch (favoriteSport) {
+  case "футбол":
+    champion = "Ліонель Мессі";
+    break;
+  case "баскетбол":
+    champion = "Леброн Джеймс";
+    break;
+  case "теніс":
+    champion = "Рафаель Надаль";
+    break;
+}
+
+if (birthYear && city && favoriteSport) {
+  message =
+    "Ваш вік: " +
+    (new Date().getFullYear() - parseInt(birthYear)) +
+    ".\n" +
+    message;
+
+  if (champion) {
+    message += "\nКруто! Хочеш стати " + champion + "?";
+  }
+
+  alert(message);
+} else {
+  let missingInputs = [];
+
+  if (!birthYear) {
+    missingInputs.push("рік народження");
+  }
+  if (!city) {
+    missingInputs.push("місто");
+  }
+  if (!favoriteSport) {
+    missingInputs.push("улюблений вид спорту");
+  }
+
+  alert(
+    "Шкода, що Ви не захотіли ввести свій(ю) " +
+      missingInputs.join(", ") +
+      "."
+  );
+}
+
